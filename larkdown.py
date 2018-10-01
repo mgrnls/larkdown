f79b0eee7bad6bc50d8050ce26c229d2a895bc25
mgrnls <42085723+mgrnls@users.noreply.github.com>
2018-10-01 17:59:33 +0200

    Update larkdown.py
    Completely rewrote code, added support for multiline mathmode

--- larkdown.py
+++ larkdown.py
@@ -12,55 +12,68 @@ def main():
     
     # if there are no files, print an error message
     if len(arguments) == 0:
-        print('No file selected')
+        print('No file(s) selected')
     
     for arg in arguments:
         to_tex(arg)
 
-def rem_spaces(s):
+
+
+def rem_spaces(input_string: str):
     # removes spaces from the beginning of a string
-    for i in range(len(s)):
-        if s[i] != ' ':
-            return s[i :]
+    for i in range(len(input_string)):
+        if input_string[i] != ' ':
+            return input_string[i :]
     return ''
-            
 
-def to_tex(file_name):
-    # takes file name, turns the file into text and then outputs a tex file file_name.tex
 
-    # beginning of tex file that we will add to     
-    top_string = '''\\documentclass{article}\n\\usepackage{enumerate}\n\\usepackage{amsfonts}\n\\usepackage{amsmath}\n\\usepackage{enumerate}\n\\usepackage[margin=1in]{geometry}\n\\setlength{\\parindent}{0in}\n\\begin{document}'''
 
-    # open the file and save contents as read_data
-    with open(file_name) as f:
-        read_data = f.read()
+def read_to_list(input_string: str):
+    # this function takes in a string and separates it into lines and mathmode environments
+    length_of_input_string = len(input_string)
     
-    # count the number of instances of #T, #A and #D respectively
-    title_count = read_data.count('#T')
-    author_count = read_data.count('#A')
-    date_count = read_data.count('#D')
+    if length_of_input_string == 1:
+        return [input_string]
     
-    # find the total number of these instances
-    total_count = title_count + author_count + date_count
+    # an integer to remember where the beginning of the current line is
+    beginning_of_line = 0
     
-    # check the document is well-formed (or sound)
-    is_sound = True
-    if title_count > 1:
-        print('Too many title fields.')
-        is_sound = False
-    if author_count > 1:
-        print('Too many author fields.')
-        is_sound = False
-    if date_count > 1:
-        print('Too many date fields.')
-        is_sound = False
     
-    # run through list, replace '_x_', '*x*' with '\textit{x}' and '\textbf{x}' respectively
+    # create an empty list that we will add to
+    output_list = []
+    
+    # a boolean which we will use to test if we are in mathmode or not
+    in_mathmode = False
+    
+    # start a counter i at 0
+    i = 0
+    
+    while i < length_of_input_string - 1:
+        if in_mathmode:
+            if input_string[i : i + 2] == '$$':
+                output_list.append(input_string[beginning_of_line : i + 2])
+                beginning_of_line = i + 2
+                in_mathmode = False
+        else:
+            if input_string[i] == '\n':
+                output_list.append(input_string[beginning_of_line : i])
+                beginning_of_line = i + 1
+            elif input_string[i : i + 2] == '$$':
+                beginning_of_line = i
+                in_mathmode = True
+        i += 1
+    output_list.append(input_string[beginning_of_line : i])
+    
+    return output_list
+
+
+
+def to_bold_italic(input_string: str):
     temp_string = ''
     italic_mode = False
     bold_mode = False
     
-    for i in read_data:
+    for i in input_string:
         if i == '_' and italic_mode == False:
             temp_string += '\\textit{'
             italic_mode = True
@@ -76,112 +89,127 @@ def to_tex(file_name):
         else:
             temp_string += i
     
-    if italic_mode == True:
-        print('Missing a _ somewhere')
-    if bold_mode == True:
-        print('Missing a * somewhere')
-    read_data = temp_string
+    if italic_mode:
+        temp_string += '}'
+    if bold_mode:
+        temp_string += '}'
+        
+    return temp_string
+
+
+
+def to_list(list_of: list, type_of: str, line_number: int, list_len: int):
+    # test if enumerate or itemize
+    if type_of == 'enumerate':
+        start_at = 2
+        testing_str = '#.'
+    elif type_of == 'itemize':
+        start_at = 1
+        testing_str = '-'
     
-    # if the document is sound, then start converting it
-    if is_sound:
-        # turn the string into a list of lines
-        data_list = read_data.split('\n')
-        
-        # set title, author and date
-        for i in data_list[0 : total_count]:
-            if i[0 : 2] == '#T':
-                title = i[3 : ]
-            if i[0 : 2] == '#A':
-                author = i[3 : ]
-            if i[0 : 2] == '#D':
-                date = i[2 : ]
-        
-        # if there is a title, author or date, then add them
-        if title_count == 1:
-            top_string += '\n\\title{' + title + '}'
-        if author_count == 1:
-            top_string += '\n\\author{' + author + '}'
-        if date_count == 1 and (date != '' or date != ' '):
-            top_string += '\n\\date{' + date + '}'
-        if date_count == 0:
-            top_string += '\n\\date{}'
-        
-        # if there is a date, author or title, then display them
-        if total_count > 0:
-            top_string += '\n\\maketitle\n'
-        
-        # removes the author, title and date parts (if any)
-        data_list = data_list[total_count : ]
-        
-        # remove all instances of '' from the data_list
-        data_list = list(filter(lambda a: a != '', data_list))
-        
-        # find size of list to use in the following loop
-        list_length = len(data_list)
-        
-        # run a loop that iterates over each line in the document
-        for i in range(list_length - 1):
-            if data_list[i][0 : 2] == '# ':
-                top_string += '\section*{' + data_list[i][2 : ] + '}\n'
-            elif data_list[i][0 : 2] == '#.':
-                if i == 0:
-                    top_string += '\\begin{enumerate}\n\\item ' + rem_spaces(data_list[i][2 : ]) + '\n'
-                    if data_list[1][0 : 2] != '#.':
-                        top_string += '\\end{enumerate}\n'
-                else:
-                    if data_list[i - 1][0 : 2] == data_list[i + 1][0 : 2] == '#.':
-                        top_string += '\\item ' + rem_spaces(data_list[i][2 : ]) + '\n'
-                    elif data_list[i - 1][0 : 2] != '#.' and data_list[i + 1][0 : 2] != '#.':
-                        top_string += '\\begin{enumerate}\n\\item ' + rem_spaces(data_list[i][2 : ]) + '\\end{enumerate}\n'
-                    elif data_list[i - 1][0 : 2] == '#.' and data_list[i + 1][0 : 2] != '#.':
-                        top_string += '\\item ' + rem_spaces(data_list[i][2 : ]) + '\n\\end{enumerate}\n'
-                    elif data_list[i - 1][0 : 2] != '#.' and data_list[i + 1][0 : 2] == '#.':
-                        top_string += '\\begin{enumerate}\n\\item ' + rem_spaces(data_list[i][2 : ]) + '\n'
-            elif data_list[i][0] == '-':
-                if i == 0:
-                    top_string += '\\begin{itemize}\n\\item ' + rem_spaces(data_list[i][1 : ]) + '\n'
-                    if data_list[1][0] != '-':
-                        top_string += '\\end{itemize}\n'
-                else:
-                    if data_list[i - 1][0] == data_list[i + 1][0] == '-':
-                        top_string += '\\item ' + rem_spaces(data_list[i][1 : ]) + '\n'
-                    elif data_list[i - 1][0] != '-' and data_list[i + 1][0] != '-':
-                        top_string += '\\begin{itemize}\n\\item ' + rem_spaces(data_list[i][1 : ]) + '\\end{itemize}\n'
-                    elif data_list[i - 1][0] == '-' and data_list[i + 1][0] != '-':
-                        top_string += '\\item ' + rem_spaces(data_list[i][1 : ]) + '\n\\end{itemize}\n'
-                    elif data_list[i - 1][0] != '-' and data_list[i + 1][0] == '-':
-                        top_string += '\\begin{itemize}\n\\item ' + rem_spaces(data_list[i][1 : ]) + '\n'
-            elif data_list[i + 1][0 : 2] == '\[' or data_list[i][0 : 2] == '\[':
-                top_string += data_list[i] + '\n'
-            elif data_list[i + 1][0 : 2] == '# ' or data_list[i + 1][0 : 2] == '#.' or data_list[i + 1][0] == '-':
-                top_string += data_list[i] + '\n'
-            else:
-                top_string += data_list[i] + '\\\\\n\n'
-        
-        # repeat for the last line
-        if data_list[list_length - 1][0 : 2] == '# ':
-            top_string += '\n\\section*{' + data_list[list_length - 1][2 : ] + '}\n'
-        elif data_list[list_length - 1][0 : 2] == '\[':
-            top_string += data_list[list_length - 1] + '\n'
-        elif data_list[list_length - 1][0 : 2] == '#.':
-            if data_list[list_length - 2][0 : 2] == '#.':
-                top_string += '\\item ' + data_list[list_length - 1][2 : ] + '\n\\end{enumerate}\n'
-            else:
-                top_string += '\\begin{enumerate}\n\\item ' + rem_spaces(data_list[list_length - 1][2 : ]) + '\n\\end{enumerate}\n'
-        elif data_list[list_length - 1][0] == '-':
-            if data_list[list_length - 2][0] == '-':
-                top_string += '\\item ' + rem_spaces(data_list[list_length - 1][1 : ]) + '\n\\end{itemize}\n'
-            else:
-                top_string += '\\begin{itemize}\n\\item ' + rem_spaces(data_list[list_length - 1][1 : ]) + '\n\\end{itemize}\n'
+    if line_number == 0:
+        output_str = '\\begin{' + type_of + '}\n\\item ' + to_bold_italic(rem_spaces(list_of[0][start_at : ])) + '\n'
+        if list_of[1][0 : start_at] != testing_str:
+            output_str += '\\end{' + type_of + '}\n'
+    elif line_number == list_len - 1:
+        if list_of[list_len - 2][0 : start_at] == testing_str:
+            output_str = '\\item ' + to_bold_italic(rem_spaces(list_of[list_len - 1][start_at : ])) + '\n\\end{' + type_of + '}\n'
         else:
-            top_string += data_list[list_length - 1]
+            output_str = '\\begin{' + type_of + '}\n\\item ' + rem_spaces(list_of[list_len - 1][start_at : ]) + '\n\\end{' + type_of + '}\n'
+    else:
+        if list_of[line_number - 1][0 : start_at] == list_of[line_number + 1][0 : start_at] == testing_str:
+            output_str = '\\item ' + to_bold_italic(rem_spaces(list_of[line_number][start_at : ])) + '\n'
+        elif list_of[line_number - 1][0 : start_at] != testing_str and list_of[line_number + 1][0 : start_at] != testing_str:
+            output_str = '\\begin{' + type_of +'}\n\\item ' + to_bold_italic(rem_spaces(list_of[line_number][start_at : ])) + '\n\\end{' + type_of + '}\n'
+        elif list_of[line_number - 1][0 : start_at] == testing_str and list_of[line_number + 1][0 : start_at] != testing_str:
+            output_str = '\\item ' + to_bold_italic(rem_spaces(list_of[line_number][start_at : ])) + '\n\\end{' + type_of +'}\n'
+        elif list_of[line_number - 1][0 : start_at] != testing_str and list_of[line_number + 1][0 : start_at] == testing_str:
+            output_str = '\\begin{' + type_of +'}\n\\item ' + to_bold_italic(rem_spaces(list_of[line_number][start_at : ])) + '\n'
+    return output_str
+
+
+
+def to_mathmode(input_string: str):
+    list_of_maths = [x for x in input_string.split('\n') if rem_spaces(x) != '']
+    
+    length_of_list = len(list_of_maths)
+    
+    if length_of_list == 1:
+        return '\\begin{align*}\n' + list_of_maths[0] + '\n\\end{align*}\n'
+    
+    output_str = '\\begin{align*}\n'
+    
+    for i in range(length_of_list - 1):
+        output_str += list_of_maths[i] + '\\\\\n'
+    output_str += list_of_maths[length_of_list - 1] + '\n\\end{align*}\n'
+    
+    return output_str
+
+
+
+def to_tex(file_name: str):
+    
+    # this is the top few lines of the tex document that we wil be adding to
+    top_string = '\\documentclass{article}\n\\usepackage{enumerate}\n\\usepackage{amsfonts}\n\\usepackage{amsmath}\n\\usepackage{enumerate}\n\\usepackage[margin=1in]{geometry}\n\\setlength{\\parindent}{0in}\n'
+    
+    content = ''
+    
+    # boolean values for whether or not to add author, title or date
+    is_author, is_title, is_date = False, False, False
+    
+    # open the file and save it as a string
+    with open(file_name) as f:
+        read_data = f.read()
+    
+    # reads the data as a list of non-empty lines
+    data_list = [x for x in read_to_list(read_data) if rem_spaces(x) != '']
+    
+    list_length = len(data_list)
+    
+    for i in range(list_length):
+        # now we list all the different cases
+        if data_list[i][0 : 2] == '#T':
+            is_title = True
+            title = rem_spaces(data_list[i][2 : ])
+        elif data_list[i][0 : 2] == '#A':
+            is_author = True
+            author = rem_spaces(data_list[i][2 : ])
+        elif data_list[i][0 : 2] == '#D':
+            is_date = True
+            date = rem_spaces(data_list[i][2 : ])
+        elif data_list[i][0 : 2] == '# ':
+            content += '\section*{' + rem_spaces(data_list[i][2 : ]) + '}\n'
+        elif data_list[i][0 : 2] == '#.':
+            content += to_list(data_list, 'enumerate', i, list_length)
+        elif data_list[i][0] == '-':
+            content += to_list(data_list, 'itemize', i, list_length)
+        elif data_list[i][0 : 2] == '$$':
+            content += to_mathmode(data_list[i][2 : -2])
+        else:
+            content += data_list[i] + '\\\\\n\n'
+    
+    if is_title:
+        top_string += '\\title{' + title + '}\n'
+    if is_author:
+        top_string += '\\author{' + author + '}\n'
+    if is_date:
+        top_string += '\\date{' + date + '}\n'
+    else:
+        top_string += '\\date{}'
+    
+    top_string += '\\begin{document}\n'
+    
+    if is_title or is_author or is_date:
+        top_string += '\\maketitle\n'
+    
+    final = top_string + content + '\\end{document}'
+    
+    # declare a new file name (only works for txt files)
+    new_file_name = file_name[ : -4] + '.tex'
         
-        # declare a new file name (only works for txt files)
-        new_file_name = file_name[ : -4] + '.tex'
-        
-        # write final tex file
-        with open(new_file_name, 'w') as f:
-            read_data = f.write(top_string + '\n\end{document}')
+    # write final tex file
+    with open(new_file_name, 'w') as f:
+        read_data = f.write(final)
 
 
 
