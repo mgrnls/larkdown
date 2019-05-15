5b8d5dc73f47a3dffaa82a2a354d68eede627d85
mgrnls <42085723+mgrnls@users.noreply.github.com>
2019-05-15 03:29:38 +0200

    Add files via upload
    removed noindent command of every line, lets latex handle indentation

--- larkdown.py
+++ larkdown.py
@@ -6,41 +6,29 @@ def main():
     """
     Main function that is run when started.
     """
-    # save all the file names as a list of strings
     arguments = sys.argv[1 : ]
-    pdflatex = False
-    
-    # if there are no files, print an error message
+    pdflatex = '-p' in arguments
+    arguments = arguments[1 : ] if pdflatex else arguments
+
     if not arguments:
         print('No files selected')
-    else:
-        if arguments[0] == '-p':
-            arguments = arguments[1 : ]
-            if not arguments:
-                print('No files selected')
-            pdflatex = True
- 
     
-    for arg in arguments:
-        to_tex(arg)
+    for argument in arguments:
+        to_tex(argument)
 
     if pdflatex:
         for arg in arguments:
             filename = arg.split('.')
             os.system('pdflatex ' + '.'.join(filename[ : -1 ]) + '.tex')
 
-
-
-def rem_spaces(input_string: str):
+def rem_spaces(input_string):
     """
     Removes spaces from the beginning of a string, returns an empty string if
     input is empty or a string containing only spaces.
     """
     return input_string.strip()
 
-
-
-def read_to_list(input_string: str):
+def read_to_list(input_string):
     """
     This function takes in a string and separates it into lines and multiline
     mathmode environments.
@@ -50,13 +38,10 @@ def read_to_list(input_string: str):
     if length_of_input_string == 1:
         return [input_string]
     
-    # an integer to remember where the beginning of the current line is
     beginning_of_line = 0
     
-    # create an empty list that we will add to
     output_list = []
     
-    # a boolean which we will use to test if we are in multiline mathmode 
     in_mathmode = False
     
     i = 0
@@ -75,20 +60,18 @@ def read_to_list(input_string: str):
                 beginning_of_line = i
                 in_mathmode = True
         i += 1
+
     output_list.append(input_string[beginning_of_line : i + 1])
     
     return output_list
 
-
-
-def to_bold_italic(input_string: str):
+def to_bold_italic(input_string):
     """
     Given a line of text, replaces the *'s and _'s with LaTeX textbf and textit
     environments respectively.
     """
     temp_string = ''
-    italic_mode = False
-    bold_mode = False
+    italic_mode, bold_mode = False, False
     
     for i in input_string:
         if i == '_' and italic_mode == False:
@@ -111,14 +94,11 @@ def to_bold_italic(input_string: str):
         
     return temp_string
 
-
-
-def to_list(list_of: list, type_of: str, line_number: int, list_len: int):
+def to_list(list_of, type_of, line_number, list_len):
     """
     Given a list of strings representing either an itemize or enumerate 
     environment, return the formatted version.
     """
-    # test if enumerate or itemize
     if type_of == 'enumerate':
         start_at = 2
         testing_str = '#.'
@@ -182,9 +162,7 @@ def to_list(list_of: list, type_of: str, line_number: int, list_len: int):
             output_str = '\\begin{' + type_of +'}\n\\item ' + middle + '\n'
     return output_str
 
-
-
-def to_mathmode(input_string: str):
+def to_mathmode(input_string):
     """
     Takes a string contained within a multiline mathmode environment and
     formats it as an align environment. If there is exactly one equals
@@ -194,7 +172,8 @@ def to_mathmode(input_string: str):
         rem_spaces(x) for x in input_string.split('\n') if rem_spaces(x)
     ]
 
-    align_equals = all(1 == x.count('=') for x in list_of_maths)
+    align_equals = all(1 == line.count('=') for line in list_of_maths)
+
     if align_equals:
         list_of_maths = [line.replace('=', '&=') for line in list_of_maths]
 
@@ -206,58 +185,55 @@ def to_mathmode(input_string: str):
     output_str = '\\begin{align*}\n'
     
     for i in range(length_of_list - 1):
-        output_str += list_of_maths[i] + '\\\\\n'
+        output_str += list_of_maths[i] + '\n'
     output_str += list_of_maths[length_of_list - 1] + '\n\\end{align*}\n'
     
     return output_str
 
-
-
-def to_tex(file_name: str):
+def to_tex(file_name):
     """
     This function is run for each file that is passed to it, and creates a
     new file with the same name but with a .tex extension.
     """
-    # this is the top few lines of the tex document that we wil be adding to
-    top_string = '\\documentclass{article}\n\\usepackage{enumerate}\n\\usepackage{amsfonts}\n\\usepackage{amsmath}\n\\usepackage{enumerate}\n\\usepackage[margin=1in]{geometry}\n\\setlength{\\parindent}{0in}\n'
+    top_string = (
+        '\\documentclass{article}\n\\usepackage{enumerate}\n\\usepackage{amsfon'
+        'ts}\n\\usepackage{amsmath}\n\\usepackage{enumerate}\n\\usepackage[marg'
+        'in=1in]{geometry}\n'
+    )
     
     content = ''
-    
-    # boolean values for whether or not to add author, title or date
+
     is_author, is_title, is_date = False, False, False
     
-    # open the file and save it as a string
     with open(file_name) as f:
         read_data = f.read()
     
-    # reads the data as a list of non-empty lines
     data_list = [x for x in read_to_list(read_data) if rem_spaces(x) != '']
     
     list_length = len(data_list)
     
     for i in range(list_length):
-        # now we list all the different cases
-        if data_list[i][0 : 2] == '#T':
+        if data_list[i][0 : 3] == '#T ':
             is_title = True
-            title = rem_spaces(data_list[i][2 : ])
-        elif data_list[i][0 : 2] == '#A':
+            title = rem_spaces(data_list[i][3 : ])
+        elif data_list[i][0 : 3] == '#A ':
             is_author = True
-            author = rem_spaces(data_list[i][2 : ])
-        elif data_list[i][0 : 2] == '#D':
+            author = rem_spaces(data_list[i][3 : ])
+        elif data_list[i][0 : 3] == '#D ':
             is_date = True
-            date = rem_spaces(data_list[i][2 : ])
+            date = rem_spaces(data_list[i][3 : ])
         elif data_list[i][0 : 2] == '# ':
             content += (
-                    '\section*{' + 
-                    to_bold_italic(rem_spaces(data_list[i][2 : ])) + 
-                    '}\n'
-                    )
+                '\section*{' + 
+                to_bold_italic(rem_spaces(data_list[i][2 : ])) + 
+                '}\n'
+            )
         elif data_list[i][0 : 3] == '## ':
             content += (
                 '\subsection*{' +
                 to_bold_italic(rem_spaces(data_list[i][3 : ])) +
                 '}\n'
-                )
+            )
         elif data_list[i][0 : 2] == '#.':
             content += to_list(data_list, 'enumerate', i, list_length)
         elif data_list[i][0] == '-':
@@ -265,7 +241,7 @@ def to_tex(file_name: str):
         elif data_list[i][0 : 2] == '$$':
             content += to_mathmode(data_list[i][2 : -2])
         else:
-            content += to_bold_italic(rem_spaces(data_list[i])) + '\\\\\n\n'
+            content += to_bold_italic(rem_spaces(data_list[i])) + '\n\n'
     
     if is_title:
         top_string += '\\title{' + title + '}\n'
@@ -283,17 +259,13 @@ def to_tex(file_name: str):
     
     final = top_string + content + '\\end{document}'
     
-    # declare a new file name (only works for txt files)
     if file_name[-4 : ] == '.txt':
         new_file_name = file_name[ : -4] + '.tex'
     elif file_name[-3 : ] == '.md':
         new_file_name = file_name[ : -3] + '.tex'
         
-    # write final tex file
     with open(new_file_name, 'w') as f:
         f.write(final)
-
-
 
 if __name__ == '__main__':
     main()
